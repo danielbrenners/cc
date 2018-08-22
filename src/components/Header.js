905e78f5cb070c314e3efbc692e1fc8d8345c447
Daniel Brenners <danielbrenners@invisionapp.com>
2018-08-21 23:23:41 -0700

    Add header and file structure

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../css/Header.css'
+
+const Header = () => {
+	return (
+		<div className="background">
+			<div className="title">CC</div>
+			<div className="subtitle">... we go hard.</div>
+		</div>
+	)
+}
+
+export default Header
